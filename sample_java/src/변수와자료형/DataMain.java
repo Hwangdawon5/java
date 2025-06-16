a2858453846562fcf4eed94ad91edcc58d716203
HwangDawon <yiouech124@naver.com>
2025-06-16 20:50:51 +0900

    .

--- sample_java/src/변수와자료형/DataMain.java
+++ sample_java/src/변수와자료형/DataMain.java
@@ -1,4 +1,18 @@
 package 변수와자료형;
-
+//변수란? 메모리 공간에 이름을 붙이는 것, 자바는 JVM이 메모리를 할당
+//변수를 만들기 위해서는 어떤 데이터를 저장할 지 결정해야 하고,
+//변수 이름은 사용 목적에 맍게 작명 해야 함
+//변수 이름의 규칙 : 영문자(대소문자), 숫자, _, $ 사용 가능
+//숫자로 시작하면 안됨, 예약어 안됨, 공백 안됨
+//camelCase 준수, 목적에 맞게 끔 이름을 작명 해야 함
 public class DataMain {
+    public static void main(String[] arge) {
+        int __1st;
+        int $$$$1st;
+        //int case; (case가 예약어)
+        //int 1st; (숫자로 시작)
+        int texRate;
+        String memberInfo;
+
+    }
 }
